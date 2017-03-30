9823dfb84e1c6feb821be9a99eaeb67ab9750f3b
BradleyCai <bradcai24@gmail.com>
2017-03-29 21:32:15 -0700

    Added documentation and primative guessing feature

--- main.py
+++ main.py
@@ -63,7 +63,12 @@ def dprint(s = "", *args):
             print(s)
 
 # Returns character for afk check
-def find_afk_check(text):
+#
+# Takes the resulting ocr string generated "text"
+# Should be in the strict format "Press [A-z] to continue playing."
+# If the string does not fit the strict format, a less strict fallback solver
+# is used.
+def solve_check(text):
     match = re.search(r'Press[ \t]+(\w)[ \t]+to[ \t]+continue[ \t]+playing.?', text, re.I)
 
     if match:
@@ -78,6 +83,15 @@ def find_afk_check(text):
             return match.group(1)
 
     return None
+
+# Returns a character guess for afk check
+# Has a json of guesses and alternatives for each character in the alphabet
+#
+# Takes the resulting character from solve_check() and guesses close
+# alternatives
+# Takes a guessed index of the current guess the script is on
+def guess_check(c, guessed):
+    return chr(ord('a') + guessed)
 
 def run_tests():
     imgs = []
@@ -98,7 +112,7 @@ def run_tests():
 
         img = enhance(img, "enhance-" + ele[1][0])
         text = image_to_string(img, lang='eng')
-        c = find_afk_check(text) # Gets character for afk check
+        c = solve_check(text) # Gets character for afk check
 
         ans = ele[1][0] # Answer is in the file name
         if (c == ans):
@@ -121,6 +135,7 @@ def main():
     img_i = 0 # afk check solve number
     delay = 0
     checkdir = "img/checks/" + time.strftime("%d%m%Y-%H%M%S")
+    guessed = 0 # Index for current guess try
 
     os.mkdir(checkdir) # Creates a unique folder for output to sit in
 
@@ -146,6 +161,7 @@ def main():
 
     if (TESTING):
         run_tests()
+        sys.exit();
 
     input("Press enter to start\n")
     if (delay > 0):
@@ -158,13 +174,17 @@ def main():
 
         img = enhance(img)
         text = image_to_string(img, lang='eng')
-        c = find_afk_check(text)
+        c = solve_check(text)
 
         if (c):
-            if (in_afk and False):
+            if (in_afk):
                 dprint("[FAILED] Failed on %d", img_i)
-                dprint("[Testing] Trying %s", "a")
-                # Test other possibilities
+
+                c = guess_check(c, guessed)
+                dprint("[Testing] Trying %s", c)
+
+                guessed += 1
+
             else:
                 dprint("[Testing] Fresh test on '%d'", img_i)
                 dprint("Got %s", c)
@@ -174,8 +194,9 @@ def main():
             img_i += 1
         else:
             if (in_afk):
-                in_afk = False
                 # clear afk try history
+                in_afk = False
+                guessed = 0
 
 if __name__ == "__main__":
     main();
